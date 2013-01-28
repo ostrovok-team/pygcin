df5bc2c362a85cfcf1814fee11b99104ab1db1ff
Yakov Borevich <jjay@ostrovok.ru>
2013-01-28 20:00:45 +0400

    Another experiment

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ DESCRIPTION = 'Python wrapper for gcin (http://github.com/4u/gcin'
 def install_deps():
     print "Installing dependencies"
     cdir = os.path.abspath(os.path.dirname(__file__))
+    os.chdir(cdir)
+    check_call(['pip', 'submodule', 'update', '--force'])
     os.chdir(os.path.join(cdir, 'pygcin/gcin'))
     check_call(['npm', 'install', '.'])
     os.chdir(cdir)
